26d2b5c2820fea576813d0cd7ac796068a43819a
무시무시한쥐 <163982251+ghastlymouse@users.noreply.github.com>
2024-06-01 20:53:54 +0900

    update : 폼에 정보 입력하고 제출 시 로컬스토리지에 정보 저장 유효성 검사 기능 추가

--- src/components/SignUp/SignUpForm.jsx
+++ src/components/SignUp/SignUpForm.jsx
@@ -1,30 +1,132 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import * as S from './SignUpForm.styled'
 
 const SignUpForm = () => {
+    const loadUsers = () => {
+        const loadedUsers = JSON.parse(localStorage.getItem("users"));
+        if (loadedUsers) {
+            return loadedUsers;
+        }
+        if (!loadedUsers) {
+            return [];
+        }
+    };
+    const [users, setUsers] = useState(loadUsers());
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [nickName, setNickName] = useState("");
+
+    const [isEmailValid, setIsEmailValid] = useState(true);
+    const [isPasswordValid, setIsPasswordValid] = useState(true);
+    const [IsPassWordConfirmValid, setIsPasswordConfirmValid] = useState(true);
+    const [isNickNameValid, setIsNickNameValid] = useState(true);
+
+    const [emailSpanMessage, setEmailSpanMessage] = useState("");
+    const [nickNameSpanMessage, setNickNameSpanMessage] = useState("");
+
+    const handleSubmitForm = (event) => {
+        event.preventDefault();
+        setIsEmailValid(true);
+        setIsPasswordValid(true);
+        setIsPasswordConfirmValid(true);
+        setIsNickNameValid(true);
+
+        if (!email.trim()) {
+            setEmailSpanMessage("이메일을 입력해주세요!");
+            return setIsEmailValid(false);
+        }
+        if (password.trim().length < 8) {
+            return setIsPasswordValid(false);
+        }
+        if (password.trim() !== passwordConfirm.trim()) {
+            return setIsPasswordConfirmValid(false);
+        }
+        if (!nickName.trim()) {
+            setNickNameSpanMessage("닉네임을 입력해주세요!");
+            return setIsNickNameValid(false);
+        }
+
+        const newUser = {
+            email,
+            password,
+            nickName,
+        };
+
+        const prevUserEmail = loadUsers().find(user => user.email === newUser.email);
+        if (prevUserEmail) {
+            setEmailSpanMessage("이미 존재하는 이메일입니다!");
+            return setIsEmailValid(false);
+        }
+
+        const prevUserNickName = loadUsers().find(user => user.nickName === newUser.nickName);
+        if (prevUserNickName) {
+            setNickNameSpanMessage("이미 존재하는 닉네임입니다!");
+            return setIsNickNameValid(false);
+        }
+
+        setUsers(prevUsers => [...prevUsers, newUser]);
+
+        setEmail("");
+        setPassword("");
+        setPasswordConfirm("");
+        setNickName("");
+
+        alert(`${nickName}님 환영합니다!`);
+    };
+
+    useEffect(() => {
+        localStorage.setItem("users", JSON.stringify(users))
+    }, [users])
+
     return (
         <>
-            <S.Form>
+            <S.Form onSubmit={handleSubmitForm}>
                 <S.InputBox>
                     <label htmlFor="email">이메일</label>
-                    <S.Input />
+                    <S.Input
+                        id="email"
+                        name="email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </S.InputBox>
-                <S.Span>이미 존재하는 이메일입니다!</S.Span>
+                <S.Span $display={isEmailValid}>{emailSpanMessage}</S.Span>
                 <S.InputBox>
                     <label htmlFor="password">비밀번호</label>
-                    <S.Input />
+                    <S.Input
+                        id="password"
+                        name="password"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </S.InputBox>
-                <S.Span>비밀번호는 최소 8자 이상 입력해주세요!</S.Span>
+                <S.Span $display={isPasswordValid}>비밀번호는 8자이상 입력해주세요!</S.Span>
                 <S.InputBox>
                     <label htmlFor="passwordConfirm">비밀번호 확인</label>
-                    <S.Input />
+                    <S.Input
+                        id="passwordConfirm"
+                        name="passwordConfirm"
+                        type="password"
+                        value={passwordConfirm}
+                        onChange={(e) => setPasswordConfirm(e.target.value)}
+                    />
                 </S.InputBox>
-                <S.Span>비밀번호를 동일하게 입력해주세요!</S.Span>
+                <S.Span $display={IsPassWordConfirmValid}>비밀번호와 동일하게 입력해주세요!</S.Span>
                 <S.InputBox>
-                    <label htmlFor="email">이메일</label>
-                    <S.Input />
+                    <label htmlFor="nickName">닉네임</label>
+                    <S.Input
+                        id="nickName"
+                        name="nickName"
+                        type="text"
+                        value={nickName}
+                        onChange={(e) => setNickName(e.target.value)}
+                    />
                 </S.InputBox>
-                <S.Span>이미 존재하는 닉네임입니다!</S.Span>
+                <S.Span $display={isNickNameValid}>{nickNameSpanMessage}</S.Span>
                 <S.Button type="submit">회원가입</S.Button>
             </S.Form>
         </>
