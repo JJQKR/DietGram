13415ff0ec5ae97c6874c24baa570018926f5f76
무시무시한쥐 <163982251+ghastlymouse@users.noreply.github.com>
2024-06-01 19:33:25 +0900

    style : SignUpForm css 수정

--- src/components/SignUp/SignUpForm.jsx
+++ src/components/SignUp/SignUpForm.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import * as S from './SignUpForm.styled'
 
 const SignUpForm = () => {
     return (
         <>
-            <Form>
-
-            </Form>
+            <S.Form>
+                <S.InputBox>
+                    <label htmlFor="email">이메일</label>
+                    <S.Input />
+                </S.InputBox>
+                <S.Span>이미 존재하는 이메일입니다!</S.Span>
+                <S.InputBox>
+                    <label htmlFor="password">비밀번호</label>
+                    <S.Input />
+                </S.InputBox>
+                <S.Span>비밀번호는 최소 8자 이상 입력해주세요!</S.Span>
+                <S.InputBox>
+                    <label htmlFor="passwordConfirm">비밀번호 확인</label>
+                    <S.Input />
+                </S.InputBox>
+                <S.Span>비밀번호를 동일하게 입력해주세요!</S.Span>
+                <S.InputBox>
+                    <label htmlFor="email">이메일</label>
+                    <S.Input />
+                </S.InputBox>
+                <S.Span>이미 존재하는 닉네임입니다!</S.Span>
+                <S.Button type="submit">회원가입</S.Button>
+            </S.Form>
         </>
     )
 }
--- src/components/SignUp/SignUpForm.styled.jsx
+++ src/components/SignUp/SignUpForm.styled.jsx
@@ -1,1 +1,42 @@
 import styled from "styled-components";
+
+export const Form = styled.form`
+    width: 1280px;
+    margin: 100px auto;
+    background-color: #E7E7E7;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    gap: 20px;
+`;
+
+export const InputBox = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const Input = styled.input`
+    width: 90%;
+    height: 60px;
+    background-color: #D9D9D9;
+    border-radius: 10px;
+`;
+
+export const Button = styled.button`
+    margin-top: 30px;
+    background-color: #0084FD;
+    color: white;
+    border: none;
+    border-radius: 15px;
+    padding: 10px 20px;
+`;
+
+export const Span = styled.span`
+    color: red;
+    display: block;
+`;
