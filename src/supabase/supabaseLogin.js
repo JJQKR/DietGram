5567c3ac813f885700ce96870670c35c8d76ba4f
무시무시한쥐 <163982251+ghastlymouse@users.noreply.github.com>
2024-06-04 13:20:32 +0900

    update : supabase 로그인 테스트용  client 생성

--- src/supabase/supabaseLogin.js
+++ src/supabase/supabaseLogin.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_PROJECT_URL = import.meta.env.VITE_SUPABASE_URL_JHU;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_KEY_JHU;
+const SUPABASE_PROJECT_URL = "https://sngxevhlxxzjbwdbherl.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuZ3hldmhseHh6amJ3ZGJoZXJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0MTc3NjIsImV4cCI6MjAzMjk5Mzc2Mn0.kyYFAIC4OPlFPHDL0j8wjcbTYixhU42jRgmfVOht5hs";
 
 const supabaseLogin = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY);
 
