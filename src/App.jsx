1621aa3acecb3667e56561a779b39a78c51fe712
oneieo <sun020716@gmail.com>
2024-06-04 13:05:50 +0900

    update : 불필요한 import 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-//import "./App.css";
-import { changeValue } from "./redux/slices/form.slice";
-import {
-  deletePost,
-  initDataList,
-  insertPost,
-} from "./redux/slices/supabase.slice";
-import SupabaseFunc from "./supabase/supabase";
 import NavBar from "./components/NavBar/NavBar";
 import Postlist from "./pages/Postlist/Postlist";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/TestPages/Home";
-import Mypage from "./pages/TestPages/Mypage";
-import Signup from "./pages/TestPages/Signup";
-import Login from "./pages/TestPages/Login";
 
 function App() {
   return (
