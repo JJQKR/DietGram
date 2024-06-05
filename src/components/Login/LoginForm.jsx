c1fef4e51f0b6ebb250f35215f09a89896b23236
무시무시한쥐 <163982251+ghastlymouse@users.noreply.github.com>
2024-06-05 10:30:53 +0900

    update : 유효성 검사 수정 및 첫 렌더링 이메일 인풋 포커스 로그인 성공 시 폼 초기화

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,27 +1,37 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { changeValue } from "../../redux/slices/form.slice";
+import { changeValue, initFormData } from "../../redux/slices/form.slice";
 import { checkLogin, getCurrentUser } from "../../redux/slices/user.slice";
 import { supabase } from "../../supabase/supabase";
+import { checkLengthValidation } from "../SignUp/signUpValidation";
 import * as S from "./LoginForm.styled";
 
 const LoginForm = () => {
-  const navigator = useNavigate();
   const dispatch = useDispatch();
-  const { currentUser } = useSelector((state) => state.user);
+  const navigator = useNavigate();
   const formData = useSelector((state) => state.formData);
-  console.log(formData);
+  const emailRef = useRef();
+  const spanRef = useRef([]);
 
-  const [isEmailValid, setIsEmailValid] = useState(true);
-  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  useEffect(() => {
+    emailRef.current.focus();
+    spanRef.current[0].style.display = "none";
+    spanRef.current[1].style.display = "none";
+  }, [])
 
   const handleSubmitLoginForm = async (event) => {
     event.preventDefault();
-    setIsEmailValid(true);
-    setIsPasswordValid(true);
-
     const { email, password } = formData;
+
+    if (!checkLengthValidation(email, 1)) {
+      dispatch(changeValue({ type: "email", content: "" }))
+      return spanRef.current[0].style.display = "block";
+    }
+    if (!checkLengthValidation(password, 6)) {
+      dispatch(changeValue({ type: "password", content: "" }))
+      return spanRef.current[1].style.display = "block";
+    }
 
     try {
       const { data, error } = await supabase.login.signInWithPassword(
@@ -33,11 +43,10 @@ const LoginForm = () => {
       } else {
         alert("로그인 되었습니다!");
         navigator("/");
-        console.log(data);
         const { user } = data;
+        dispatch(initFormData());
         dispatch(getCurrentUser(user));
         dispatch(checkLogin(true));
-        console.log(currentUser);
       }
     } catch (error) {
       alert("네트워크 이슈");
@@ -52,6 +61,7 @@ const LoginForm = () => {
           <S.Input
             id="email"
             type="email"
+            ref={emailRef}
             onChange={(e) => {
               const action = changeValue({
                 type: "email",
@@ -61,7 +71,7 @@ const LoginForm = () => {
             }}
           />
         </S.InputBox>
-        <S.Span $display={isEmailValid}>
+        <S.Span ref={el => (spanRef.current[0] = el)}>
           이메일을 정확히 입력한 것이 맞는지 확인해주세요!
         </S.Span>
         <S.InputBox>
@@ -78,7 +88,7 @@ const LoginForm = () => {
             }}
           />
         </S.InputBox>
-        <S.Span $display={isPasswordValid}>비밀번호를 확인해주세요!</S.Span>
+        <S.Span ref={el => (spanRef.current[1] = el)}>비밀번호를 확인해주세요!</S.Span>
         <S.ButtonDiv>
           <S.Button type="submit">로그인</S.Button>
           <S.Button $color="green" type="button" onClick={() => navigator("/signup")}>
