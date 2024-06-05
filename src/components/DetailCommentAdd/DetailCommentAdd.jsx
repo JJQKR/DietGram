f8b34cfbbe8632cbfd5449d37fd6a9a700e30078
KANGJIHYEON1121 <wlgus4083@gmail.com>
2024-06-05 19:49:35 +0900

    update: 좋아요 댓글 등록 유효성 검사

--- src/components/DetailCommentAdd/DetailCommentAdd.jsx
+++ src/components/DetailCommentAdd/DetailCommentAdd.jsx
@@ -6,6 +6,7 @@ import { CommentAddButton, CommentInput, DetailPostCommentInput } from './Detail
 
 const DetailCommentAdd = () => {
   const dispatch = useDispatch();
+  const isLogin = useSelector((state) => state.user.isLogin);
   const comment = useSelector((state) => state.formData.comment);
   const userInfo = useSelector((state) => state.user.currentUser?.user_metadata);
   const curPostId = useSelector((state) => state.posts.currentPostId);
@@ -14,6 +15,7 @@ const DetailCommentAdd = () => {
   // console.log('commentList', commentList);
 
   const completeChange = async () => {
+    if (isLogin === false) return alert('로그인 해주세요.');
     const data = await supabase.comment.insertComment(curPostId, comment, userInfo.avatarUrl, userInfo.nickName);
     const action = insertData(data);
     dispatch(action);
--- src/components/PsotLike/PostLike.jsx
+++ src/components/PsotLike/PostLike.jsx
@@ -4,31 +4,23 @@ import { supabase } from '../../supabase/supabase';
 import { useState } from 'react';
 
 const PostLike = () => {
-  const dispatch = useDispatch();
   const [like, setLike] = useState(-1);
+  const currentUserLogin = useSelector((state) => state.user.isLogin);
   const likeList = useSelector((state) => state.posts?.postList);
   const curPost = useSelector((state) => state.posts?.currentPostId);
   const curPostUser = useSelector((state) => state.user?.currentUser);
-  console.log(curPost);
 
   const handleLikeClick = async (id, userId) => {
+    if (currentUserLogin === false) return alert('로그인을 해주세요.');
     const data = await supabase.post.isLike(id, userId);
     console.log(data); // 데이터를 이용하여서 아래 하트만 다르게 표시
     setLike(data?.like.findIndex((item) => item === curPost));
   };
-  console.log(like);
-  // console.log(curPost);
-  // console.log(likeList);
-  // console.log(
-  //   likeList.filter((postLike) => {
-  //     return postLike.id === curPost;
-  //   })
-  // );
 
   return (
     <PostLikeBox>
       <PostLikeButton onClick={() => handleLikeClick(curPost, curPostUser?.id)}>
-        {like == -1 ? '♡' : '♥︎'}
+        {like === -1 ? '♡' : '♥︎'}
       </PostLikeButton>
       <span>좋아요 갯수</span>
     </PostLikeBox>
