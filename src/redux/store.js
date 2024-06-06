aae99a6cc8a3315b6c9588b84916df0ae46822e8
Joseph Park <rkfnahs12e@gmail.com>
2024-06-06 22:03:10 +0900

    fix : persist middleware가 직렬화 어쩌고 저쩌고

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
-// import supabaseSlice from './slices/supabase.slice';
-// import currentUserSlice from './slices/currentUser.slice';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
 import persistReducer from 'redux-persist/es/persistReducer';
 import persistStore from 'redux-persist/es/persistStore';
 import storage from 'redux-persist/lib/storage';
@@ -20,13 +19,20 @@ const rootReducer = combineReducers({
   comments: commentsSlice,
   formData: formSlice,
   activeIndex: activeIndexSilce,
-  // currentUser: currentUserSlice,
   user: userSlice
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = configureStore({ reducer: persistedReducer });
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+});
 
 const persistor = persistStore(store);
 
